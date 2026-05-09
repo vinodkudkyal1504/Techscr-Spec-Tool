5a047df44bdbf9e9a7632835b185319a70626100
vinodkudkyal1504 <vinodkudkyal1504@gmail.com>
2026-05-09 21:01:12 +0530

    Cors Restricted

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://techscr-spec-tool-frontend.onrender.com",
+  }),
+);
 app.use(express.json({ limit: "2mb" }));
 
 // Serve frontend
